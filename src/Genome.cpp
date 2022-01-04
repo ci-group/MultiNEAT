d83f7a8b21ccc34d91c104d85a7295e3310e15f9
Aart Stuurman <aart@astuurman.com>
2022-01-04 09:16:41 +0100

    Format

--- src/Genome.cpp
+++ src/Genome.cpp
@@ -2616,8 +2616,11 @@ namespace NEAT
         // for all neurons..
         for (unsigned int i = 0; i < NumNeurons(); i++)
         {
-            // skip inputs and bias
-            if ((!a_Parameters.MutateOutputActivationFunction || m_NeuronGenes[i].Type() != OUTPUT) && (m_NeuronGenes[i].Type() != INPUT) && (m_NeuronGenes[i].Type() != BIAS))
+            // skip inputs and bias and output is that setting is enabled
+            if (!(a_Parameters.MutateOutputActivationFunction &&
+                    m_NeuronGenes[i].Type() == OUTPUT) &&
+                (m_NeuronGenes[i].Type() != INPUT) &&
+                (m_NeuronGenes[i].Type() != BIAS))
             {
                 double t_randnum = a_RNG.RandFloatSigned() * a_Parameters.ActivationAMutationMaxPower;
 
@@ -2637,8 +2640,11 @@ namespace NEAT
         // for all neurons..
         for (unsigned int i = 0; i < NumNeurons(); i++)
         {
-            // skip inputs and bias
-            if ((!a_Parameters.MutateOutputActivationFunction || m_NeuronGenes[i].Type() != OUTPUT) && (m_NeuronGenes[i].Type() != INPUT) && (m_NeuronGenes[i].Type() != BIAS))
+            // skip inputs and bias and output is that setting is enabled
+            if (!(a_Parameters.MutateOutputActivationFunction &&
+                    m_NeuronGenes[i].Type() == OUTPUT) &&
+                (m_NeuronGenes[i].Type() != INPUT) &&
+                (m_NeuronGenes[i].Type() != BIAS))
             {
                 double t_randnum = a_RNG.RandFloatSigned() * a_Parameters.ActivationBMutationMaxPower;
 
@@ -2659,7 +2665,9 @@ namespace NEAT
         int t_first_idx = NumInputs();
         int t_choice = a_RNG.RandInt(t_first_idx, m_NeuronGenes.size() - 1);
 
-        if (a_Parameters.MutateOutputActivationFunction && m_NeuronGenes[t_choice].Type() == OUTPUT)
+        // do not mutate if this neuron is an output neuron if that setting is enabled
+        if (a_Parameters.MutateOutputActivationFunction &&
+            m_NeuronGenes[t_choice].Type() == OUTPUT)
         {
             return false;
         }
