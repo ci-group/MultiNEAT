c0b00881c67b38be774475b284893df224311f2b
Aart Stuurman <aartstuurman@hotmail.com>
2022-06-09 10:21:24 +0200

    Restore part of old tests

--- tests/serialization/serialize_genome.cpp
+++ tests/serialization/serialize_genome.cpp
@@ -39,4 +39,17 @@ BOOST_AUTO_TEST_CASE(serialize_genome)
     genome_loaded2.Deserialize(serialized);
 
     BOOST_TEST(genome == genome_loaded2);
+
+    // below are old tests, keeping them as extra
+    BOOST_TEST(genome.GetID() == genome_loaded2.GetID());
+    BOOST_TEST(genome.m_LinkGenes == genome_loaded2.m_LinkGenes);
+    BOOST_TEST(genome.m_NeuronGenes == genome_loaded2.m_NeuronGenes);
+    BOOST_TEST(genome.GetDepth() == genome_loaded2.GetDepth());
+    BOOST_TEST(genome.NumInputs() == genome_loaded2.NumInputs());
+    BOOST_TEST(genome.NumOutputs() == genome_loaded2.NumOutputs());
+    BOOST_TEST(genome.GetFitness() == genome_loaded2.GetFitness());
+    BOOST_TEST(genome.GetAdjFitness() == genome_loaded2.GetAdjFitness());
+    BOOST_TEST(genome.GetDepth() == genome_loaded2.GetDepth());
+    BOOST_TEST(genome.GetOffspringAmount() == genome_loaded2.GetOffspringAmount());
+    BOOST_TEST(genome.m_Evaluated == genome_loaded2.m_Evaluated);
 }
